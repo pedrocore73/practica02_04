4cbd0bce1e7a09823cba38d7095db4d646749aca
pedrocore73 <pjimenez@corenetworks.es>
2019-10-29 17:26:10 +0100

    martes

--- src/app/servicios/rbac.service.ts
+++ src/app/servicios/rbac.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,36 @@ import { environment } from 'src/environments/environment';
 export class RbacService {
 
   urlLogin = environment.urlLogin;
+  usuario: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private router: Router) {
+    this.cargarEstado();
+  }
+
+  cargarEstado() {
+    let id = localStorage.getItem('id');
+    if(id){
+      this.router.navigate(['/inicio']);
+    }
+  }
 
   login(credenciales) {
     return this.http.post(this.urlLogin, credenciales).pipe(
       map((res: any)=>{
-        console.log(res);
+        if(res.usuario) {
+          this.usuario = res.usuario;
+          localStorage.setItem('id', this.usuario._id);
+        }
         return res;
       })
     )
+  }
+
+  logout() {
+    this.usuario = {};
+    localStorage.removeItem('id');
+    this.router.navigate(['/']);
   }
 
 }
